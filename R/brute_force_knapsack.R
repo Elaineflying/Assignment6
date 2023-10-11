17b8e1b2c6f5ad08aaa249081be8b69306ab1eca
Elaine <flytosky1012@gmail.com>
2023-10-11 22:46:26 +0200

    add brute and dymanic solutions

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -4,7 +4,7 @@
 #' @param x a data frame which contains two variables v and w, stands for each items value and weight respectively.
 #' @param W a postive integer which stands for the knapsack size
 #' @returns the maximum knapsack value and which elements.
-#' @examples 
+#' @examples
 #' RNGversion(min(as.character(getRversion()),"3.5.3"))
 #' set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
 #' n <- 2000
@@ -14,12 +14,32 @@
 #' v=runif(n = n, 0, 10000)
 #' )
 #' brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
-#' @import plyr
-#' @import ggplot2
-#' @import methods
 #' @export brute_force_knapsack
-brute_force_knapsack <-
-function(x,w) {
-    return(value)
-    return(elements)  
+brute_force_knapsack <- function(x,W) {
+  # Checking x argument
+  if ( !(is.data.frame(x) && any(x > 0)) ) {
+    stop("The argument x should be a data frame with two variables v and w, with only positive values")
+  }
+  # Checking W argument
+  if ( !(is.numeric(W) && W > 0)) {
+    stop("The argument W should be a postive integer, please check!")
+  }
+  num_items <- nrow(x)
+  max_value <- 0
+  best_elements <-numeric(num_items)
+
+  # Generate all possible binary combination for item
+  for ( i in 0:(2^num_items -1)) {
+    binary_search <- as.integer(intToBits(i))[1:num_items]
+    if ( sum(binary_search * x$w) <= W ) {
+      current_value <- sum(binary_search * x$v)
+      if (current_value > max_value) {
+        max_value <- current_value
+        best_elements <- binary_search
+      }
+    }
+  }
+  value <- round(max_value)
+  elements <- which(best_elements==1)
+  return(list(value=value, elements=elements))
 }
---
+++ R/dynamic_knapsack.R
@@ -0,0 +1,56 @@
+#' Dynamic search should return the same results as the brute force algorithm when solving the knapsack problem.
+#' @references Reference page link <https://en.wikipedia.org/wiki/Knapsack problem#0.2F1 knapsack problem>
+#' @description Dynamic search return the same results as brute force algorithm, but it should scale much better with O(W n) runing speed.
+#' @param x a data frame which contains two variables v and w, stands for each items value and weight respectively.
+#' @param W a postive integer which stands for the knapsack size
+#' @returns the maximum knapsack value and which elements.
+#' @examples
+#' RNGversion(min(as.character(getRversion()),"3.5.3"))
+#' set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+#' n <- 2000
+#' knapsack_objects <-
+#' data.frame(
+#' w=sample(1:4000, size = n, replace = TRUE),
+#' v=runif(n = n, 0, 10000)
+#' )
+#' dynamic_knapsack(x = knapsack_objects[1:8,], W = 3500)
+#' @export dynamic_knapsack
+dynamic_knapsack <- function(x,W) {
+  # Checking x argument
+  if ( !(is.data.frame(x) && any(x > 0)) ) {
+    stop("The argument x should be a data frame with two variables v and w, with only positive values")
+  }
+  # Checking W argument
+  if ( !(is.numeric(W) && W > 0)) {
+    stop("The argument W should be a postive integer, please check!")
+  }
+  num_items <- nrow(x)
+  #create a dynamic programming table to store the maximum values
+  x_mat <- matrix(0, nrow = num_items + 1, ncol = W + 1)
+  for (i in 1:num_items) {
+    for ( j in 0:W) {
+      if ( x[i, "w"] <= j) {
+        x_mat[i + 1, j] <- max(
+          x_mat[i, j],
+          x_mat[i, j - x[i, "w"]] + x[i, "v"]
+        )
+      } else {
+        x_mat[i + 1, j] <- x_mat[i, j]
+      }
+    }
+  }
+  # trace back to find the best elements
+  best_elements <- numeric(num_items)
+  i <- num_items
+  j <- W
+  while ( i > 0 && j > 0) {
+    if (x_mat[i + 1,j] != x_mat[i, j]) {
+      best_elements[i] <- 1
+      j <- j - x[i, "w"]
+    }
+    i <- i - 1
+  }
+  value <- round(x_mat[num_items + 1, W])
+  elements <- which(best_elements==1)
+  return(list(value=value, elements=elements))
+}
