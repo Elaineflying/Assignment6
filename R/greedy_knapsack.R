d757e4bbcd7100b82af5cb493b0d91cd2c21f89a
AshenWELI <ashenset@gmail.com>
2023-10-13 01:18:49 +0200

    adding test cases for greedy and brute force

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -13,6 +13,15 @@
 #' @export greedy_knapsack
 # Greedy knapsack algorithm
 greedy_knapsack <- function(x, W) {
+
+  # Checking x argument
+  if ( !(is.data.frame(x) && any(x > 0)) ) {
+    stop("The argument x should be a data frame with two variables v and w, with only positive values")
+  }
+  # Checking W argument
+  if ( !(is.numeric(W) && W > 0)) {
+    stop("The argument W should be a postive integer, please check!")
+  }
   # Sort the items in decreasing order of value-to-weight ratio.
   x <- x[order(x[, 2] / x[, 1], decreasing = TRUE), ]
   value<-0
@@ -25,16 +34,19 @@ greedy_knapsack <- function(x, W) {
   # Iterate over the items in sorted order.
   for (i in 1:nrow(x)) {
     item <- x[i, ]
+
+    # Choosing integer values for if loop
     item_weight <- as.integer(item[1])
     item_value <- as.integer(item[2])
 
     # If the item fits in the knapsack, add it.
     if (item_weight + weight <= W) {
-      knapsack$value <- knapsack$value + item_value
+      knapsack$value <- knapsack$value + item_value  # Store the values
       knapsack$elements <- c(knapsack$elements, i)  # Store the index of the item.
-      weight <- weight + item_weight
+      weight <- weight + item_weight   # Store the weights
     }
   }
 
   return(knapsack)
 }
+
