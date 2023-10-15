a362ce5379c6e5ccd29e0e93dc5aef89e9d09f49
Elaine <flytosky1012@gmail.com>
2023-10-15 20:31:06 +0200

    fix document

--- R/greedy_knapsack.R
+++ R/greedy_knapsack.R
@@ -5,6 +5,14 @@
 #' @param W a postive integer which stands for the knapsack size
 #' @returns the maximum knapsack value and which elements.
 #' @examples
+#' RNGversion(min(as.character(getRversion()),"3.5.3"))
+#' set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+#' n <- 2000
+#' knapsack_objects <-
+#' data.frame(
+#' w=sample(1:4000, size = n, replace = TRUE),
+#' v=runif(n = n, 0, 10000)
+#' )
 #' greedy_knapsack(x = knapsack_objects[1:800,], W = 3500)
 #' greedy_knapsack(x = knapsack_objects[1:1200,], W = 2000)
 #' @export greedy_knapsack
