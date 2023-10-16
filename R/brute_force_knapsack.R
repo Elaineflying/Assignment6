67906e2ba54ddbab49f698bd9c8753ae9e382fbb
AshenWELI <ashenset@gmail.com>
2023-10-16 18:31:43 +0200

    Profile creating for brute force knapsack

--- R/brute_force_knapsack.R
+++ R/brute_force_knapsack.R
@@ -43,3 +43,30 @@ brute_force_knapsack <- function(x,W) {
   elements <- which(best_elements==1)
   return(list(value=value, elements=elements))
 }
+
+RNGversion(min(as.character(getRversion()),"3.5.3"))
+set.seed(42, kind = "Mersenne-Twister", normal.kind = "Inversion")
+n <- 2000
+knapsack_objects <-data.frame(
+  w=sample(1:4000, size = n, replace = TRUE),
+  v=runif(n = n, 0, 10000)
+)
+
+
+library(profvis)
+p<-profvis::profvis({
+  interval=0.01
+
+
+  brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+
+})
+
+
+print(p)
+system.time(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500))
+
+
+
+
+
